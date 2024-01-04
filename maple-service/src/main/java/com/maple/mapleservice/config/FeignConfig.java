ddfdda84f29b1443b2f4512770270dc04535f7a3
graywhales <zzaeuk@gmail.com>
2024-01-05 00:19:34 +0900

    [Feat] feign Logger 추가 level BASIC feign Client 로 API콜하는지 로그 확인용

--- maple-service/src/main/java/com/maple/mapleservice/config/FeignConfig.java
+++ maple-service/src/main/java/com/maple/mapleservice/config/FeignConfig.java
@@ -1,13 +1,19 @@
 package com.maple.mapleservice.config;
 
 import com.maple.mapleservice.util.APIRequestInterceptor;
+import feign.Logger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 
 @Configuration
 public class FeignConfig {
     @Bean
     public APIRequestInterceptor feignInterceptor() {
         return new APIRequestInterceptor();
+    }
+    @Bean
+    Logger.Level feighnLoggerLevel(){
+        return Logger.Level.BASIC;
     }
 }
