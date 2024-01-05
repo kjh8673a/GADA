dc74937a090ff5a08f8b24fc0abaddf36a20fd17
jihoon_gweon <kjh8673a@naver.com>
2024-01-05 18:01:26 +0900

    [Setting] redis sentinel

--- maple-service/src/main/java/com/maple/mapleservice/config/RedisConfig.java
+++ maple-service/src/main/java/com/maple/mapleservice/config/RedisConfig.java
@@ -1,6 +1,7 @@
 package com.maple.mapleservice.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
@@ -8,16 +9,23 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisSentinelConfiguration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
+import lombok.RequiredArgsConstructor;
+
 @Configuration
 @EnableCaching
+@RequiredArgsConstructor
 public class RedisConfig {
     @Value("${spring.data.redis.host}")
     private String host;
@@ -25,13 +33,15 @@ public class RedisConfig {
     @Value("${spring.data.redis.port}")
     private int port;
 
+    private final RedisProperties redisProperties;
+
 //    추후 비밀번호 설정시 추가...RedisConnectionFactory 부분도 변경
 //    @Value("${spring.data.redis.password}")
 //    private String password;
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory(){
-        return new LettuceConnectionFactory(host,port);
+        return new LettuceConnectionFactory(host, port);
     }
 
     @Bean
@@ -57,4 +67,12 @@ public class RedisConfig {
         return builder.build();
     }
 
+    @Bean
+    public RedisTemplate<?, ?> redisTemplate() {
+        RedisTemplate<byte[], byte[]> template = new RedisTemplate<>();
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
+        template.setConnectionFactory(redisConnectionFactory());
+        return template;
+    }
 }
