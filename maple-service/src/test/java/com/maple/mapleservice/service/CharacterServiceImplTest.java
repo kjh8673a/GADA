740f468873dff2ea9d4068fa0c8bb5ca296c56cc
graywhales <zzaeuk@gmail.com>
2024-01-07 18:22:50 +0900

    [Test] 기본정보,인기도,하이퍼스탯 조회 테스트 추가

--- maple-service/src/test/java/com/maple/mapleservice/service/CharacterServiceImplTest.java
+++ maple-service/src/test/java/com/maple/mapleservice/service/CharacterServiceImplTest.java
@@ -1,21 +1,15 @@
 package com.maple.mapleservice.service;
 
+import com.maple.mapleservice.dto.feign.character.CharacterBasicDto;
 import com.maple.mapleservice.dto.feign.character.CharacterStatDto;
-import com.maple.mapleservice.entity.character.FinalStat;
-import org.assertj.core.api.Assertions;
-import org.junit.Rule;
-import org.junit.jupiter.api.BeforeEach;
+import com.maple.mapleservice.entity.character.HyperStat;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.system.OutputCaptureRule;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
-
 
 @SpringBootTest
 class CharacterServiceImplTest {
@@ -24,6 +18,7 @@ class CharacterServiceImplTest {
 
 //    @Rule
 //    public OutputCaptureRule output = new OutputCaptureRule();
+    private String ocid = "e0a4f439e53c369866b55297d2f5f4eb";
 
     @Test
     void getOcidKey() {
@@ -33,13 +28,30 @@ class CharacterServiceImplTest {
 
     @Test
     void 캐시_로그_테스트(){
-        String key = characterService.getOcidKey("하이");
+        String key = characterService.getOcidKey("아델");
         //나중에 테스트 확인 코드 넣자... outputcapturerule 이 적용이 잘안됨
     }
     @Test
-    void 스탯_api_테스트(){
-        String ocid = characterService.getOcidKey("도적");
+    void 기본정보_조회_테스트(){
+        CharacterBasicDto characterBasicDto = characterService.getCharacterBasic(ocid);
+        assertThat(characterBasicDto.getCharacter_class()).isEqualTo("아델");
+    }
+    @Test
+    void 인기도_조회_테스트(){
+        Integer characterPopularity = characterService.getCharacterPopularity(ocid);
+        assertThat(characterPopularity).isInstanceOf(Integer.class);
+    }
+    @Test
+    void 종합_능력치_조회_테스트(){
         CharacterStatDto characterStatDto = characterService.getCharacterStat(ocid);
         assertThat(characterStatDto.getFinal_stat().size()).isEqualTo(44);
+    }
+
+    @Test
+    void 하이퍼스탯_조회_테스트(){
+        List<HyperStat> characterHyperStat = characterService.getCharacterHyperStat(ocid);
+        for (HyperStat hyperStat : characterHyperStat) {
+            System.out.println(hyperStat.getStat_type()+": "+hyperStat.getStat_level()+" :"+hyperStat.getStat_increase());
+        }
     }
 }
