94e6d7fd318b5840b375e72b5fe247a510912357
graywhales <zzaeuk@gmail.com>
2024-01-07 20:15:27 +0900

    [Feat] 어빌리티 엔티티 추가

---
+++ maple-service/src/main/java/com/maple/mapleservice/entity/character/Ability.java
@@ -0,0 +1,10 @@
+package com.maple.mapleservice.entity.character;
+
+import lombok.Data;
+
+@Data
+public class Ability {
+    String ability_no;
+    String ability_grade;
+    String ability_value;
+}
