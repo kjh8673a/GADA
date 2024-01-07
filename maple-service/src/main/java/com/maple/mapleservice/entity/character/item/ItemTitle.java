611c01b9ff509d72a35411d8bbf48b0a0f5d8994
graywhales <zzaeuk@gmail.com>
2024-01-07 20:15:02 +0900

    [Feat] 아이템 엔티티 추가

---
+++ maple-service/src/main/java/com/maple/mapleservice/entity/character/item/ItemTitle.java
@@ -0,0 +1,12 @@
+package com.maple.mapleservice.entity.character.item;
+
+import lombok.Data;
+
+@Data
+public class ItemTitle {
+    String title_name;
+    String title_icon;
+    String title_description;
+    String date_expire;
+    String date_option_expire;
+}
