45030afe19dfbd5f0e2e3e3fc2e8940f39493009
graywhales <zzaeuk@gmail.com>
2024-01-07 20:15:16 +0900

    [Feat] 캐시아이템 엔티티 추가

---
+++ maple-service/src/main/java/com/maple/mapleservice/entity/character/cashitem/CashItemColoring.java
@@ -0,0 +1,11 @@
+package com.maple.mapleservice.entity.character.cashitem;
+
+import lombok.Data;
+
+@Data
+public class CashItemColoring {
+    String color_range;
+    Integer hue;
+    Integer saturation;
+    Integer value;
+}
---
+++ maple-service/src/main/java/com/maple/mapleservice/entity/character/cashitem/CashItemOption.java
@@ -0,0 +1,9 @@
+package com.maple.mapleservice.entity.character.cashitem;
+
+import lombok.Data;
+
+@Data
+public class CashItemOption {
+    String option_type;
+    String option_value;
+}
