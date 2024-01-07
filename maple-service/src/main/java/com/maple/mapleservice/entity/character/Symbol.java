098eb6ab086d72dd6ccb99007e8e11a4717cb489
graywhales <zzaeuk@gmail.com>
2024-01-07 20:15:53 +0900

    [Feat] 펫장비,심볼 엔티티 추가

---
+++ maple-service/src/main/java/com/maple/mapleservice/entity/character/Symbol.java
@@ -0,0 +1,19 @@
+package com.maple.mapleservice.entity.character;
+
+import lombok.Data;
+
+@Data
+public class Symbol {
+    String symbol_name;
+    String symbol_icon;
+    String symbol_description;
+    String symbol_force;
+    Integer symbol_level;
+    String symbol_str;
+    String symbol_dex;
+    String symbol_int;
+    String symbol_luk;
+    String symbol_hp;
+    Integer symbol_growth_count;
+    Integer symbol_require_growth_count;
+}
