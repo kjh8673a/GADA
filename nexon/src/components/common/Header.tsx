ded99dfae59592766e85dd2c57b034fadf711d7b
ssjjhh <jbson1998@naver.com>
2024-01-09 00:33:19 +0900

    [Feat] Header와 Footer추가

--- nexon/src/components/common/Header.tsx
+++ nexon/src/components/common/Header.tsx
@@ -1,18 +1,48 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {
+    Domain, DownConatiner, HeaderContainer,
+    InputHeaderName, UpContainer, PageHeader
+} from '../../style/header';
+import { useNavigate } from 'react-router-dom';
 
-interface Props {
-    children: React.ReactNode;
-}
 
-const Header: React.FC<Props> = ({children}) => {
+const Header = () => {
+    const [headerName, setHeaderName] = useState<string>('');
+    const navigate = useNavigate();
 
-    const Click = () => {
-        
+    //헤더에 있는 Input창에 닉네임 또는 길드명 입력
+    const changeInputValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+        console.log(e.target.value);
+        setHeaderName(e.target.value);
     }
+
+    //헤더에 있는 Input창에 Enter를 눌렀을 때.
+    const headerInputEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        //엔터 클릭시 이름가지고 SearchPage로 이동해야함.
+        if (e.key === 'Enter') {
+            // navigate("/Search");
+            console.log(headerName);
+        }
+    }
+
+
+    
     return (
-        <>
-            {children}
-        </>
+        <HeaderContainer>
+            <UpContainer>
+                <Domain href='/'>GADA</Domain>
+                <InputHeaderName placeholder='닉네임 또는 길드명 입력'
+                    onChange={changeInputValue}
+                    onKeyDown={headerInputEnter} />
+            </UpContainer>
+            <DownConatiner>
+                <PageHeader href='/'>메인</PageHeader>
+                <PageHeader href='/rank'>랭킹</PageHeader>
+                <PageHeader href='/vs'>캐릭터 비교</PageHeader>
+                {/* <PageHeader>재획 타이머</PageHeader>
+                <PageHeader>파티구성</PageHeader> */}
+            </DownConatiner>
+        </HeaderContainer>
     )
 }
 
---
+++ nexon/src/style/footer.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const FooterContainer = styled.div`
+    display : flex;
+    flex-direction : column;
+    font-size : 12px;
+    position : fixed;
+    bottom: 0;
+    width : 100%;
+    background-color : #757575;
+    z-index : 1000;
+`
