bf1c8d2a7e5478d56d5acd0b6618049374e9c72b
jihoon_gweon <kjh8673a@naver.com>
2024-01-14 14:01:23 +0900

    [Test] 캐릭터 경험치 히스토리 테스트코드 수정

--- maple-service/src/test/java/com/maple/mapleservice/service/character/CharacterServiceTest.java
+++ maple-service/src/test/java/com/maple/mapleservice/service/character/CharacterServiceTest.java
@@ -60,7 +60,7 @@ class CharacterServiceTest {
 	@Test
 	void 경험치_히스토리_삽입_테스트() {
 		String ocid = "e0a4f439e53c369866b55297d2f5f4eb";
-		if (!characterExpHistoryRepository.getLatestExpDate(ocid).equals(commonUtil.date)){
+		if (characterExpHistoryRepository.countByOcid(ocid) == 0){
 			characterServiceImpl.addCharacterExpHistoryFirstTime(ocid);
 		}
 
