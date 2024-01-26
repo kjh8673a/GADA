eeac82899cfce1eb481c012f0765c881ccfdbd5c
Kwon <power916@daum.net>
2024-01-26 16:55:23 +0900

    [Fix] 유니온 배치도 오류 수정

--- nexon/src/hooks/maple/useCharacterUnion.ts
+++ nexon/src/hooks/maple/useCharacterUnion.ts
@@ -26,18 +26,19 @@ const useCharacterUnion = () => {
               );
               data.data.union_block.forEach((v: UnionBlockType) => {
                 v.block_position?.forEach((v) => {
-                  grid[10 - v.y][v.x + 11] = true;
+                  if (0 <= 10 - v.y && 20 > 10 - v.y && 0 <= v.x + 11 && 22 > v.x + 11) {
+                    grid[10 - v.y][v.x + 11] = true;
+                  }
                 });
               });
               return grid;
             });
           }
-        })
-        .catch((res) => {
+        }).catch((res) => {
           if (res.response.status === 500) {
-            console.log("Error getCharacterUnion");
-          }
-        });
+            // console.log("Error getCharacterUnion");
+          };
+        })
     },
     [setcharacterUnion, setUnionGrid]
   );
