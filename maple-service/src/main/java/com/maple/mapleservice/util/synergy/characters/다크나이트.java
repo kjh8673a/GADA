8e69eda3981426c607fe45e21228863c4fa3f3d4
jihoon_gweon <kjh8673a@naver.com>
2024-01-29 22:54:26 +0900

    [Feat] 나이트로드~미하일 시너지 스킬 작성

--- maple-service/src/main/java/com/maple/mapleservice/util/synergy/characters/다크나이트.java
+++ maple-service/src/main/java/com/maple/mapleservice/util/synergy/characters/다크나이트.java
@@ -11,43 +11,35 @@ import lombok.Getter;
 public class 다크나이트 {
 	String character_class = "다크나이트";
 	Integer cycle = 3;
-	List<String> skill_type = List.of("HP/MP 상승", "버프 해제");
+	List<String> skill_type = List.of("딜상승", "버프 해제");
 	List<SynergySkill> skill_desc = List.of(
 		new SynergySkill(
-			"퍼지 에어리어",
-			"주위에 결계를 펼쳐 결계 안으로 들어오는 적들의 공격력과 이동속도, 방어율을 감소시킨다. "
-				+ "추가로 영구히 보스 사냥 시 추가 데미지를 줄 수 있다.",
-			"MP 41 소비, 40초 동안 지속, 결계 안의 보스 몬스터를 제외한 적의 공격력 및 방어율 30% 감소, 이동속도 -60 [패시브 효과 : 보스 몬스터 공격 시 데미지 10% 증가]",
-			"https://open.api.nexon.com/static/maplestory/SkillIcon/KEKDLHPBMC.png"
+			"비홀더스 버프",
+			"자신과 파티원에게 비홀더가 일정 시간마다 버프를 사용한다.",
+			"4초 마다 지속시간이 100초인 공격력과 마력 40, 방어력 500, 크리티컬 확률 10% 증가 버프 시전\n파티원에게는 절반만 적용",
+			"https://open.api.nexon.com/static/maplestory/SkillIcon/KFPDIGOBNG.png"
 		),
 		new SynergySkill(
-			"쇼다운 챌린지",
-			"최대 6명의 적을 공격하며 도발하여 부딪히지 않는 상태로 만든다. "
-				+ "도발당한 적은 경험치 획득량과 아이템 드롭율도 함께 증가하며 보스에게도 절반의 효과가 적용된다. "
-				+ "부적이 폭발할 때 어둠의 수리검이 생성되어 주변의 적을 추적하여 공격한다. "
-				+ "어둠의 수리검은 공격반사 상태의 적을 공격해도 피해를 입지 않는다.",
-			"MP 50, 표창 3개 소비 "
-				+ "최대 6명의 적 708% 데미지로 2번 공격하고 6개의 수리검을 생성하며 도발 "
-				+ "도발당한 적은 부딪히지 않은 상태 지속. 140초 동안 획득 경험치, 아이템 드롭률 5% 증가, 보스일 경우 효과 절반 "
-				+ "수리검은 24% 데미지로 6번 공격을 하며 한 명의 적이 여러 개의 수리검에 맞을 경우 두 번째 수리검부터 최종 데미지 50% 감소, 일반 몬스터 공격 시 데미지 200% 증가 "
-				+ "수리검 생성 후 재생성까지 대기시간 5초",
-			"https://open.api.nexon.com/static/maplestory/SkillIcon/KEKDLHPBMC.png"
+			"매직 크래쉬",
+			"일정 확률로 주변 적들에게 적용된 일부 버프를 해제하며, 지정된 시간 동안 버프 효과가 적용될 수 없도록 제한한다.\\n공격을 받은 적은 90초 동안 매직 크래쉬 상태 이상에 저항한다. 해제되는 버프는 공격력/마력 증가, 방어력 증가, 하드 스킨이다.",
+			"MP 15 소비, 100% 확률로 일부 버프 해제, 22초 동안 버프 효과 제한\\n재사용 대기시간 60초",
+			"https://open.api.nexon.com/static/maplestory/SkillIcon/KFPDIFPBNE.png"
 		)
 	);
 
 	IncreaseVolume increaseVolume = IncreaseVolume.builder()
-		.plus_str(30)
-		.plus_dex(30)
-		.plus_int(30)
-		.plus_luk(30)
-		.plus_hp(30)
+		.plus_str(0)
+		.plus_dex(0)
+		.plus_int(0)
+		.plus_luk(0)
+		.plus_hp(0)
 		.multiply_str(1.0)
 		.multiply_dex(1.0)
 		.multiply_int(1.0)
 		.multiply_luk(1.0)
 		.multiply_hp(1.0)
-		.plus_attack_power(0)
-		.plus_magic_power(0)
+		.plus_attack_power(20)
+		.plus_magic_power(20)
 		.multiply_attack_power(1.0)
 		.multiply_magic_power(1.0)
 		.plus_boss_damage(0)
--- maple-service/src/main/java/com/maple/mapleservice/util/synergy/characters/데몬_슬레이어.java
+++ maple-service/src/main/java/com/maple/mapleservice/util/synergy/characters/데몬_슬레이어.java
@@ -11,27 +11,25 @@ import lombok.Getter;
 public class 데몬_슬레이어 {
 	String character_class = "데몬 슬레이어";
 	Integer cycle = 2;
-	List<String> skill_type = List.of("바인드", "힐", "드롭률");
+	List<String> skill_type = List.of("드롭률", "바인드", "힐");
 	List<SynergySkill> skill_desc = List.of(
 		new SynergySkill(
-			"퍼지 에어리어",
-			"주위에 결계를 펼쳐 결계 안으로 들어오는 적들의 공격력과 이동속도, 방어율을 감소시킨다. "
-				+ "추가로 영구히 보스 사냥 시 추가 데미지를 줄 수 있다.",
-			"MP 41 소비, 40초 동안 지속, 결계 안의 보스 몬스터를 제외한 적의 공격력 및 방어율 30% 감소, 이동속도 -60 [패시브 효과 : 보스 몬스터 공격 시 데미지 10% 증가]",
-			"https://open.api.nexon.com/static/maplestory/SkillIcon/KEKDLHPBMC.png"
+			"데빌 크라이",
+			"순간적으로 잠재된 힘을 방출하여 사방의 적 다수를 위협하고 강한 데미지를 준다. 대상은 암흑 상태 이상에 걸려 방어력, 공격력, 명중률이 감소하며 경험치 획득량과 아이템 드롭 확률이 증가한다.\\n인피니티 포스 지속 중에는 재사용 대기시간이 감소한다.",
+			"HP 400, 포스 30 소비, 최대 15명의 적을 515% 데미지로 7번 공격하며 위협. 대상은 60초 동안 공격력, 방어력 15%, 명중률 20% 감소, 획득 경험치, 아이템 드롭률 20% 증가\\n재사용 대기시간 14초, 인피니티 포스 지속 중에는 7초로 감소",
+			"https://open.api.nexon.com/static/maplestory/SkillIcon/KFMDKFPBMD.png"
 		),
 		new SynergySkill(
-			"쇼다운 챌린지",
-			"최대 6명의 적을 공격하며 도발하여 부딪히지 않는 상태로 만든다. "
-				+ "도발당한 적은 경험치 획득량과 아이템 드롭율도 함께 증가하며 보스에게도 절반의 효과가 적용된다. "
-				+ "부적이 폭발할 때 어둠의 수리검이 생성되어 주변의 적을 추적하여 공격한다. "
-				+ "어둠의 수리검은 공격반사 상태의 적을 공격해도 피해를 입지 않는다.",
-			"MP 50, 표창 3개 소비 "
-				+ "최대 6명의 적 708% 데미지로 2번 공격하고 6개의 수리검을 생성하며 도발 "
-				+ "도발당한 적은 부딪히지 않은 상태 지속. 140초 동안 획득 경험치, 아이템 드롭률 5% 증가, 보스일 경우 효과 절반 "
-				+ "수리검은 24% 데미지로 6번 공격을 하며 한 명의 적이 여러 개의 수리검에 맞을 경우 두 번째 수리검부터 최종 데미지 50% 감소, 일반 몬스터 공격 시 데미지 200% 증가 "
-				+ "수리검 생성 후 재생성까지 대기시간 5초",
-			"https://open.api.nexon.com/static/maplestory/SkillIcon/KEKDLHPBMC.png"
+			"다크 바인드",
+			"일정 확률로 주변의 적 다수를 행동 불가 상태로 만든다. 추가로 몬스터의 방어율을 일정수준만큼 무시하는 패시브 효과가 있으며 다크 바인드의 효과는 보스에게도 동일하게 적용된다. \\n공격을 받은 적은 90초 동안 행동 불가 상태 이상에 저항하여 다크 바인드 및 다른 스킬로 인한 행동 불가 상태 이상에 걸리지 않는다.",
+			"포스 60 소비, 최대 15명의 적을 700%의 데미지로 3번 공격, 대상은 10초 동안 100%확률로 행동 불가 상태 적용\\n다크 바인드로 적에게 준 데미지에 따라 행동 불가 지속시간 최대 100% 증가\\n재사용 대기시간 120초\\n[패시브 효과 : 영구적으로 몬스터의 방어율 30% 무시]",
+			"https://open.api.nexon.com/static/maplestory/SkillIcon/KFMDKFPBMG.png"
+		),
+		new SynergySkill(
+			"뱀피릭 터치",
+			"일정 시간 동안 적들에게 입힌 데미지의 일부를 나와 파티원의 HP로 회복시킨다.",
+			"포스 30 소비, 180초 동안 공격 데미지의 3%를 파티원의 HP로 회복시킴. 최대 HP의 25%이상 회복 불가, 한 번 흡수 후 5초 동안 재흡수 불가",
+			"https://open.api.nexon.com/static/maplestory/SkillIcon/KFMDKFPBMC.png"
 		)
 	);
 
@@ -41,11 +39,11 @@ public class 데몬_슬레이어 {
 		.plus_int(0)
 		.plus_luk(0)
 		.plus_hp(0)
-		.multiply_str(1.2)
-		.multiply_dex(1.2)
-		.multiply_int(1.2)
-		.multiply_luk(1.2)
-		.multiply_hp(1.2)
+		.multiply_str(1.0)
+		.multiply_dex(1.0)
+		.multiply_int(1.0)
+		.multiply_luk(1.0)
+		.multiply_hp(1.0)
 		.plus_attack_power(0)
 		.plus_magic_power(0)
 		.multiply_attack_power(1.0)
