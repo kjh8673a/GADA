c05e93980e482c9abd127a826e54da52b39bc101
PakaOxO <arpeggiogio@gmail.com>
2024-02-03 21:34:22 +0900

    [Feat] GA, 페이지 별 추적 타이틀 수정

--- nexon/src/components/common/RouterTrackerForGA.tsx
+++ nexon/src/components/common/RouterTrackerForGA.tsx
@@ -1,6 +1,16 @@
 import { PropsWithChildren, useEffect, useState } from "react";
 import ga4 from "react-ga4";
 import { useLocation } from "react-router-dom";
+import { extractInfoFromUrl } from "../../hooks/commonUtils";
+
+const PAGE_NAME: { [key: string]: string } = {
+  "": "메인 페이지",
+  character: "캐릭터 조회",
+  rank: "랭킹 조회",
+  comparison: "캐릭터 비교",
+  search: "길드&캐릭터 검색",
+  party: "파티 구성",
+};
 
 const RouterTrackerForGA = ({ children }: PropsWithChildren) => {
   const location = useLocation();
@@ -8,8 +18,8 @@ const RouterTrackerForGA = ({ children }: PropsWithChildren) => {
 
   // 구글 애널리틱스 운영서버만 적용
   useEffect(() => {
-    if (process.env.REACT_APP_GOOGLE_ANALYTICS) {
-      // if (process.env.REACT_APP_GOOGLE_ANALYTICS && !window.location.href.includes("localhost")) {
+    // if (process.env.REACT_APP_GOOGLE_ANALYTICS) {
+    if (process.env.REACT_APP_GOOGLE_ANALYTICS && !window.location.href.includes("localhost")) {
       ga4.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS);
       setInitialized(true);
     }
@@ -18,8 +28,15 @@ const RouterTrackerForGA = ({ children }: PropsWithChildren) => {
   // location 변경 감지시 pageview 이벤트 전송
   useEffect(() => {
     if (initialized) {
-      ga4.set({ page: location.pathname });
-      ga4.send("pageview");
+      const extracted = extractInfoFromUrl(location.pathname) || "";
+      const pageTitle = PAGE_NAME[extracted];
+
+      ga4.send({
+        hitType: "pageview",
+        path: location.pathname,
+        location: location.pathname,
+        title: pageTitle,
+      });
     }
   }, [initialized, location]);
 
