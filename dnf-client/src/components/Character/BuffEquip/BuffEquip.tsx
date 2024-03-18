4193665b21209f3ff09e3bb498570aa5e3da039b
Kwon <power916@daum.net>
2024-03-18 23:31:27 +0900

    [Fix] 버프강화장비없는 유저 예외처리

--- dnf-client/src/components/Character/BuffEquip/BuffEquip.tsx
+++ dnf-client/src/components/Character/BuffEquip/BuffEquip.tsx
@@ -12,26 +12,30 @@ const BuffEquip = () => {
   const data = useRecoilValue(atomCharacterBuffEquip);
   return (
     <ComponentBox flexDirection={"column"}>
-      {data.skillInfo?.option?.level ? (
-        <>
-          <BuffSkillInfo skillInfo={data.skillInfo} />
-          {data.equipment?.map((v, i) => {
-            return <BuffEquipItem key={i} data={v} />;
-          })}
-          {data.avatar?.map((v, i) => {
-            return <BuffAvartarItem key={i} data={v} />;
-          })}
-          {data.creature?.map((v, i) => {
-            return <BuffCreatureItem key={i} data={v} />;
-          })}
-        </>
+      {data ? (
+        data.skillInfo?.option?.level ? (
+          <>
+            <BuffSkillInfo skillInfo={data.skillInfo} />
+            {data.equipment?.map((v, i) => {
+              return <BuffEquipItem key={i} data={v} />;
+            })}
+            {data.avatar?.map((v, i) => {
+              return <BuffAvartarItem key={i} data={v} />;
+            })}
+            {data.creature?.map((v, i) => {
+              return <BuffCreatureItem key={i} data={v} />;
+            })}
+          </>
+        ) : (
+          <Loading
+            text={
+              "최근 1년이내 접속기록이 없는 캐릭터는 버프 강화 장비를 조회할 수 없습니다."
+            }
+            play={false}
+          />
+        )
       ) : (
-        <Loading
-          text={
-            "최근 1년이내 접속기록이 없는 캐릭터는 버프 강화 장비를 조회할 수 없습니다."
-          }
-          play={false}
-        />
+        <Loading text={"장착된 버프 강화 장비가 없습니다."} play={false} />
       )}
     </ComponentBox>
   );
