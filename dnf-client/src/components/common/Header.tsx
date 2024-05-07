09b9cda1157a150bbb8f15117e0324779584365a
Kwon <power916@daum.net>
2024-05-07 23:24:27 +0900

    [Feat] 사이트 링크 드롭다운 버튼

--- dnf-client/src/components/common/Header.tsx
+++ dnf-client/src/components/common/Header.tsx
@@ -1,6 +1,37 @@
-import { Domain, DownConatiner, UpContainer, PageHeader, DomainLogo } from "../../style/header";
+import React, { useEffect, useRef, useState } from "react";
+import ArrowSvg from "../../style/ArrowSvg";
+import {
+  Domain,
+  DownConatiner,
+  UpContainer,
+  PageHeader,
+  DomainLogo,
+} from "../../style/header";
+import DropDownList from "./DropDownList";
+import { DropDownBtn } from "../../style/dropDown";
 
 const Header = () => {
+  const [isClick, setIsClick] = useState<boolean>(false);
+  const buttonRef = useRef<HTMLDivElement>(null);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClick = (e: MouseEvent) => {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(e.target as Node) &&
+        !buttonRef.current?.contains(e.target as Node)
+      ) {
+        setIsClick(false);
+      }
+    };
+    window.addEventListener("mousedown", handleClick);
+    return () => window.removeEventListener("mousedown", handleClick);
+  }, [dropdownRef, buttonRef, setIsClick]);
+
+  const dropDownClickHandler = () => {
+    setIsClick((prev) => !prev);
+  };
 
   return (
     <>
@@ -11,8 +42,18 @@ const Header = () => {
             e.preventDefault();
           }}
         >
-          <DomainLogo src={`${process.env.PUBLIC_URL}/assets/gadalogo.webp`} alt="site logo" />
+          <DomainLogo
+            src={`${process.env.PUBLIC_URL}/assets/gadalogo.webp`}
+            alt="site logo"
+          />
         </Domain>
+        <div style={{ position: "relative" }}>
+          <DropDownBtn onClick={dropDownClickHandler} ref={buttonRef}>
+            <img src={"/assets/dnf_logo.svg"} alt="dnf logo" width={50} />
+            <ArrowSvg isClick={isClick} />
+          </DropDownBtn>
+          {isClick && <DropDownList ref={dropdownRef} />}
+        </div>
       </UpContainer>
       <DownConatiner>
         <PageHeader href="/">캐릭터 검색</PageHeader>
